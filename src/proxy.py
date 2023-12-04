969036e93923b14814ef11f2ea79d9b922f874b8
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-12-04 13:34:36 +0100

    add proxy

---
+++ src/proxy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from manager import QueueClient
+
+
+class Proxy(QueueClient, BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        t = self.task_queue.get()
+        self.wfile.write(bytes(t.to_json(), "utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=Proxy):
+    server_address = ("", 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
