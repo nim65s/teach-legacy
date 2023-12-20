265d5852b8f21fcbc0947158e9995019250f8850
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-12-20 15:58:52 +0100

    proxy: allow POST

--- src/proxy.py
+++ src/proxy.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from json import dumps
 from manager import QueueClient
+
+from task import Task
 
 
 class Proxy(BaseHTTPRequestHandler):
@@ -15,6 +18,16 @@ class Proxy(BaseHTTPRequestHandler):
         self.end_headers()
         t = self.client.task_queue.get()
         self.wfile.write(bytes(t.to_json(), "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        content_length = int(self.headers.get("content-length"))
+        content = self.rfile.read(content_length)
+        t = Task.from_json(content.decode())
+        t = self.client.result_queue.put(t)
+        self.wfile.write(bytes(dumps({"status": "ok"}), "utf-8"))
 
 
 def run(server_class=HTTPServer, handler_class=Proxy):
