fd39d1f25be3e61a5da1ca0f2e8c07983af7cf3e
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-01 22:25:44 +0200

    index only current year

--- index.py
+++ index.py
@@ -19,13 +19,14 @@ for f in Path().rglob("*.md"):
     data[year][cursus].append(f.stem)
 
 
-for year, content in sorted(data.items()):
+for year, content in sorted(data.items(), reverse=True):
     html += f'\n<h2 id="{year}">{year}</h2>\n'
     for cursus, slides in sorted(content.items()):
         html += f'<h3 id="{year}-{cursus}">{cursus}</h3>\n<ul>\n'
         for slide in sorted(slides):
             html += f'<li><a href="{year}/{cursus}/{slide}.pdf">{slide}.pdf</a></li>'
         html += "\n</ul>\n"
+    break
 
 
 html += '<hr>\n<a href="src">src</a>\n</body>\n</html>'
