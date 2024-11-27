97c169b7b18789ef189ad6d51694da4bf1115f4e
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-27 13:42:54 +0100

    flake add nix2container

--- flake.nix
+++ flake.nix
@@ -2,14 +2,23 @@
   description = "Teaching";
 
   inputs = {
+    nix2container ={
+      url = "github:nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
     laas-beamer-theme = {
       url = "git+https://gitlab.laas.fr/gsaurel/laas-beamer-theme";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
